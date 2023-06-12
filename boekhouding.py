c4f9e01ced308c88846656d5860c5b3715913750
Vera Florizoone <vfl@beddeleem.be>
2023-06-12 11:23:04 +0200

    csv functie aangepast

--- boekhouding.py
+++ boekhouding.py
@@ -16,5 +16,5 @@ print(presenteer)
 
 with open('boekhouding.csv', 'w',newline='') as csvfile:
     for key, value in inkomsten.items():
-        writer = csv.writer(csvfile, delimter=';')
+        writer = csv.writer(csvfile, delimiter=';')
         writer.writerow([key,value])
